1335e482a17325f0ada09e89d9dd89be57b9d671
Nachociro <145990857+Nachociro@users.noreply.github.com>
2024-09-12 12:02:13 -0300

    funciones de dividir y multiplicar eliminando el hello

--- dividir.py
+++ dividir.py
@@ -1,8 +1,8 @@
-n1=float(input("Ingrese un numero: "))
-n2=float(input("Ingrese otro numero: "))
+numero1=float(input("Ingrese el numero a dividir: "))
+numero2=float(input("Ingrese el numero divisor: "))
 
-def dividir(nro, nro2):#aca hice la funcion
+def dividir(nro, nro2):
     total=nro/nro2
     print("el total de la division es: ",round(total,2))
 
-dividir(n1,n2)
+dividir(numero1,numero2)
--- multiplicar.py
+++ multiplicar.py
@@ -1,10 +1,8 @@
-n1=int(input("Ingrese un numero: "))
-n2=int(input("Ingrese otro numero: "))
+numero1=int(input("Ingrese el primer numero para multiplicar: "))
+numero2=int(input("Ingrese el segundo numero para multiplicar: "))
 
 def multiplicar(nro, nro2):
     total=nro*nro2
     print("el total de la multiplicacion es: ",total)
 
-multiplicar(n1,n2)
-
-
+multiplicar(numero1,numero2)
